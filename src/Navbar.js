f39c68356da02bbda6d5d4e4fe7e49b037ad2b49
amriarendy <amriarendy90@gmail.com>
2023-07-08 00:56:42 +0700

    first commit reactjs

---
+++ src/Navbar.js
@@ -0,0 +1,31 @@
+// 1. class components rcc
+// import React, { Component } from 'react'
+
+// export default class Navbar extends Component {
+//   render() {
+//     return (
+//       <div>Navbar</div>
+//     )
+//   }
+// }
+
+// 2. function components rfc
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+
+// 3. const components rafc
+// import React from 'react'
+
+// const Navbar = () => {
+//   return (
+//     <div>Navbar</div>
+//   )
+// }
+
+// export default Navbar;
